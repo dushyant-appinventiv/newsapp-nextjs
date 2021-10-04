8895cf042d9b40a0bc6babcd95575db8fd84d58f
dushyant-appinventiv <dushyant.goyal@appinventiv.com>
2021-10-04 16:43:44 +0530

    added domains for external url images

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "gadgets360cdn.com",
+      "images.news18.com",
+      "images.cnbctv18.com",
+      "st1.bgr.in",
+      "i.gadgets360cdn.com",
+      "fdn.gsmarena.com",
+      "images.hindustantimes.com",
+      "akm-image-a-in.tosshub.com",
+      "notebookcheck.net",
+      "english.cdn.zeenews.com",
+      "static.toiimg.com",
+      "img.republicworld.com",
+      "images.moneycontrol.com",
+      "c.ndtvimg.com",
+    ],
+  },
+};
